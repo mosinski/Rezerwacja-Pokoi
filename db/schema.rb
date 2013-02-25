b7a17992b7a788663dd85e7b98a3baacee4bd443
Miłosz Osiński <mylosz@yahoo.pl>
2013-02-25 19:07:13 +0100

    proba poprawy

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130222204108) do
+ActiveRecord::Schema.define(:version => 20130222205652) do
 
   create_table "images", :force => true do |t|
     t.string   "nazwa"
     t.text     "opis"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "user_sessions", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
