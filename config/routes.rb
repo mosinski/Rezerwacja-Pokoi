5f5486e7a785e10d610fb495ab2e0bd9307a4115
Miłosz Osiński <mylosz@yahoo.pl>
2013-02-25 18:15:18 +0100

    autentykacja + kilka poprawek

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 PokojeBialogora::Application.routes.draw do
+ resources :users, :user_sessions
+ match 'login' => 'user_sessions#new', :as => :login
+ match 'logout' => 'user_sessions#destroy', :as => :logout
+ match 'logowanie' => 'user_sessions#new', :as => :logout
+ match 'rejestracja' => 'users#new'
+
   resources :images
 
   resources :websites
